49204818b8e21a9c13ec72b31c1eff092c994bd4
Davi <davi.mendonca@outlook.com>
2021-07-25 17:25:04 -0300

    Criando um pequeno serviço para listar a empresas cadastradas
    Novo pequeno serviço que retorna a lista de empresas cadastradas de acordo com a requisição do client (JSON ou XML).

---
+++ src/main/java/br/com/alura/gerenciador/servlet/EmpresasService.java
@@ -0,0 +1,64 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.gerenciador.modelo.Banco;
+import br.com.alura.gerenciador.modelo.Empresa;
+
+/**
+ * Servlet implementation class EmpresasService
+ */
+@WebServlet("/empresas")
+public class EmpresasService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		Banco banco = new Banco();
+
+		List<Empresa> empresas = banco.getEmpresas();
+
+		String tipoRetorno = request.getHeader("Accept");
+
+		if (tipoRetorno.contains("application/json")) {
+
+			/* RETORNANDO UM JSON */
+
+			Gson gson = new Gson();
+
+			String json = gson.toJson(empresas);
+
+			response.setContentType("application/json");
+			response.getWriter().print(json);
+		
+		} else if (tipoRetorno.contains("application/xml")) {
+
+			/* RETORNANDO UM XML */
+
+			XStream xstream = new XStream();
+			xstream.alias("empresa", Empresa.class);
+
+			String xml = xstream.toXML(empresas);
+
+			response.setContentType("application/xml");
+			response.getWriter().print(xml);
+		
+		} else {
+			response.setContentType("application/json");
+			response.getWriter().print("{'message' : 'no content!'}");
+		}
+
+	}
+
+}
