6425ca7e332d211c7088326c4cf54cde6d0cf1ca
Davi <davi.mendonca@outlook.com>
2021-07-25 13:41:07 -0300

    Implementando uma cadeia de filtros(Filters)
    Implementação dos filtros:
    - MonitoramentoFilter
    - AutorizacaoFilter
    - ControladorFilter

---
+++ src/main/java/br/com/alura/gerenciador/servlet/AutorizacaoFilter.java
@@ -0,0 +1,46 @@
+package br.com.alura.gerenciador.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet Filter implementation class AutorizacaoFilter
+ */
+//@WebFilter("/entrada")
+public class AutorizacaoFilter implements Filter {
+
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
+		
+		System.out.println("AutorizacaoFilter");
+	
+		HttpServletRequest request = (HttpServletRequest) servletRequest;
+		HttpServletResponse response = (HttpServletResponse) servletResponse;
+		
+		String paramAcao = request.getParameter("acao");
+		
+		if(paramAcao.equals("Cadastrar")) {
+			System.out.println("login: " + request.getParameter("login") + "  senha: " + request.getParameter("senha"));
+		}
+		
+		HttpSession sessao = request.getSession();
+		boolean logado = sessao.getAttribute("usuarioLogado") != null;
+		boolean acaoProtegida = !(paramAcao.equals("LoginForm") || paramAcao.equals("Login") || paramAcao.equals("CadastrarUsuarioForm") || paramAcao.equals("Cadastrar"));
+		
+		if(acaoProtegida && !logado) {
+			response.sendRedirect("entrada?acao=LoginForm");
+			return;
+		}
+		
+		chain.doFilter(request, response);
+	}
+
+}
--- src/main/java/br/com/alura/gerenciador/servlet/ControladorServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/ControladorServlet.java
@@ -8,52 +8,38 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import br.com.alura.gerenciador.acao.Acao;
 
 // CONTROLADOR RESPONSAVEL POR DIRECIONAR AS REQUISIÇÕES
 
-@WebServlet(urlPatterns = "/entrada")
+//@WebServlet(urlPatterns = "/entrada")
 public class ControladorServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		String paramAcao = request.getParameter("acao");
-		
-		if(paramAcao.equals("Cadastrar")) {
-			System.out.println("login: " + request.getParameter("login") + "  senha: " + request.getParameter("senha"));
-		}
-		
-		HttpSession sessao = request.getSession();
-		boolean logado = sessao.getAttribute("usuarioLogado") != null;
-		boolean acaoProtegida = !(paramAcao.equals("LoginForm") || paramAcao.equals("Login") || paramAcao.equals("CadastrarUsuarioForm") || paramAcao.equals("Cadastrar"));
-		
-		if(acaoProtegida && !logado) {
-			response.sendRedirect("entrada?acao=LoginForm");
-			return;
-		}
-		
-		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
-		
-		String nome;
-		try {
-			Class classe = Class.forName(nomeDaClasse);
-			Acao acao = (Acao) classe.newInstance();
-			nome = acao.executa(request, response);
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-			throw new ServletException(e);
-		}
-		
-		String[] dispatchOrRedirect = nome.split(":");
-		if (dispatchOrRedirect[0].equals("forward")) {
-			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/" + dispatchOrRedirect[1]);
-			rd.forward(request, response);
-		} else {
-			response.sendRedirect("entrada?acao=" + dispatchOrRedirect[1]);
-		}
+//		String paramAcao = request.getParameter("acao");
+//		
+//		String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
+//		
+//		String nome;
+//		try {
+//			Class classe = Class.forName(nomeDaClasse);
+//			Acao acao = (Acao) classe.newInstance();
+//			nome = acao.executa(request, response);
+//		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+//			throw new ServletException(e);
+//		}
+//		
+//		String[] dispatchOrRedirect = nome.split(":");
+//		if (dispatchOrRedirect[0].equals("forward")) {
+//			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/" + dispatchOrRedirect[1]);
+//			rd.forward(request, response);
+//		} else {
+//			response.sendRedirect("entrada?acao=" + dispatchOrRedirect[1]);
+//		}
 		
 	}
 
